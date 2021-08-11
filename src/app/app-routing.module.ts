41ef15ae86567f0e6847c96cd7494d9c626dbeb4
gwynt1 <gabrneg@gmail.com>
2021-08-10 20:46:53 -0400

    Adições de telas e assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login-register',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'login-register',
+    loadChildren: () => import('./login-register/login-register.module').then( m => m.LoginRegisterPageModule)
   }
 ];
 
